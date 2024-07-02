9f2c2b53a35abe6d01f54ee9a993fb957bc125b9
Naing-Bhone-Htet <67512467+Naing-Bhone-Htet@users.noreply.github.com>
2024-07-02 22:17:38 +0800

    Add files via upload

---
+++ Number Guesser/number_guesser.py
@@ -0,0 +1,45 @@
+import random
+
+titletext = "Welcome to Number Guesser Game "
+ch = '-'
+sp = ' '
+print(f'{ch * (len(titletext) + 40)}')
+print(f'*{sp * 19}{titletext}{sp * 19}*')
+print(f'{ch * (len(titletext) + 40)}')
+print()
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+        
+else:
+    print ("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+
+while True:
+    guesses += 1
+    user_guess  = input("make a guess:")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+    
+    if user_guess == random_number:
+        print("You got it right!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+        
+print(f'You got it in {guesses} guesses')
